775f6f9008de7f80372604d6023895b17bcc7914
Amit Saini <amitssaini999@gmail.com>
2024-10-25 21:29:01 +0530

    change folder name

---
+++ backend/Models/transaction.model.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  dateOfSale: Date,
+  category: String,
+  sold: Boolean,
+});
+
+const transactionModel = mongoose.model('transaction', transactionSchema);
+module.exports=transactionModel;
