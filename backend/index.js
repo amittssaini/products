e01331f4491aa334664d1a4ce7e0ddb976ec4f8d
Amit Saini <amitssaini999@gmail.com>
2024-10-25 21:21:44 +0530

    connections

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const 
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose
+.connect(process.env.DB_URI)
+.then(()=>console.log(`DB IS CONNECTED ${process.env.DB_URI}`))
+.catch((e)=>console.log(`DB IS NOT CONNECTED ${e}`))
+
+app.listen(process.env.PORT,()=>console.log('SERVER IS LISTEN AT THE PORT ',process.env.PORT))
